208f6ffb2e008f70de7fbdb835fe3eba2dc0c641
KAVINRAJA AR <kavinraja.250194@cse.ritchennai.edu.in>
2026-09-10 21:48:39 +0530

    Update vite config to allow localtunnel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    https: true,
+    allowedHosts: true,
   },
   resolve: {
     alias: {
